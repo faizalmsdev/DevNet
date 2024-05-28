98206a9e0ef9c01984eba21e59131294bef5386a
Mohamed Faizal <faizalmohamed.vi@gmail.com>
2024-05-28 16:54:40 +0530

    template

--- app/index.jsx
+++ app/index.jsx
@@ -5,19 +5,19 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Hello</Text>
       <StatusBar style="auto" />
       <Link href='/profile' style={{color:"blue"}}>Go to profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
