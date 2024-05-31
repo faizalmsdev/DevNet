87aa96ae7033aabdead0c76979163f3f5bd31f25
Mohamed Faizal <faizalmohamed.vi@gmail.com>
2024-05-31 17:14:43 +0530

    completed home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,39 @@
 import { Slot } from 'expo-router';
 import React from 'react';
-import { StyleSheet, View, StatusBar, Text } from 'react-native';
+import { StyleSheet, View, StatusBar, Text, ScrollView, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import {images} from "../constants"
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Hello</Text>
-      <StatusBar style="auto" />
-      <Link href='/home' style={{color:"blue"}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height:'100%'}}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image 
+            source={images.logo}
+            className="w-[130px] h-[84px] "
+            resizeMode='contain'
+          />
+
+          <Image 
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode='contain'
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">Discover Endless Possibilities with {' '} <Text className="text-secondary-200">Aora</Text></Text>
+            <Image 
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode='contain'
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">When creativity meets innovation: embark on a journey of limitless exploration with Aura</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
