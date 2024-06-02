91837175f4ee68e91c1009266d3fc67c9e1a0d8f
Dextrodark <faizalmohamed5302002@gmail.com>
2024-06-03 04:37:43 +0530

    added keyboard view in sign up

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import { View, Text, ScrollView, Image, Alert, KeyboardAvoidingView } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link, useRouter } from 'expo-router';
@@ -49,7 +49,6 @@ const SignUp = () => {
     setIsSignUpSubmitting(true);
 
     // Simulate API call and navigate to verification screen
-    // Simulate API call and navigate to verification screen
     setTimeout(() => {
       setIsSignUpSubmitting(false);
       // Navigate to the verification page
@@ -60,60 +59,62 @@ const SignUp = () => {
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
-          <View style={{ alignItems: 'center' }}>
-            <Image
-              source={images.logo}
-              resizeMode="contain"
-              style={{ width: 115, height: 35 }}
-            />
-          </View>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <ScrollView>
+          <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={images.logo}
+                resizeMode="contain"
+                style={{ width: 115, height: 35 }}
+              />
+            </View>
 
-          <Text className="text-2xl text-secondary text-semibold mt-10 font-psemibold">
-            Sign Up for DevNet
-          </Text>
-
-          <FormField
-            title="Username"
-            value={signUpForm.username}
-            handleChangeText={(e) => setSignUpForm({ ...signUpForm, username: e })}
-            otherStyles="mt-7"
-          />
-
-          <FormField
-            title="Phone Number/Email"
-            value={signUpForm.contact}
-            handleChangeText={(e) => setSignUpForm({ ...signUpForm, contact: e })}
-            otherStyles="mt-7"
-            keyboardType="email-address"
-          />
-
-          <FormField
-            title="Password"
-            value={signUpForm.password}
-            handleChangeText={(e) => setSignUpForm({ ...signUpForm, password: e })}
-            otherStyles="mt-7"
-            secureTextEntry={true}
-          />
-
-          <CustomButton
-            title="Sign Up"
-            handlePress={submit}
-            containerStyles="mt-7"
-            isLoading={isSignUpSubmitting}
-          />
-
-          <View className="justify-center pt-5 flex-row gap-2">
-            <Text className="text-lg text-gray-100 font-pregular">
-              Already have an account?
+            <Text className="text-2xl text-secondary text-semibold mt-10 font-psemibold">
+              Sign Up for DevNet
             </Text>
-            <Link href="/auth/sign-in" className="text-lg font-psemibold text-secondary">
-              Sign In
-            </Link>
+
+            <FormField
+              title="Username"
+              value={signUpForm.username}
+              handleChangeText={(e) => setSignUpForm({ ...signUpForm, username: e })}
+              otherStyles="mt-7"
+            />
+
+            <FormField
+              title="Phone Number/Email"
+              value={signUpForm.contact}
+              handleChangeText={(e) => setSignUpForm({ ...signUpForm, contact: e })}
+              otherStyles="mt-7"
+              keyboardType="email-address"
+            />
+
+            <FormField
+              title="Password"
+              value={signUpForm.password}
+              handleChangeText={(e) => setSignUpForm({ ...signUpForm, password: e })}
+              otherStyles="mt-7"
+              secureTextEntry={true}
+            />
+
+            <CustomButton
+              title="Sign Up"
+              handlePress={submit}
+              containerStyles="mt-7"
+              isLoading={isSignUpSubmitting}
+            />
+
+            <View className="justify-center pt-5 flex-row gap-2">
+              <Text className="text-lg text-gray-100 font-pregular">
+                Already have an account?
+              </Text>
+              <Link href="/auth/sign-in" className="text-lg font-psemibold text-secondary">
+                Sign In
+              </Link>
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
